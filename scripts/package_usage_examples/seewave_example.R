14160efbf89de31e067fda241e5831a282507535
Colin G. Wilson <colingwilson7@gmail.com>
2019-06-10 10:40:42 -0400

    Added seewave usage example

--- scripts/package_usage_examples/seewave_example.R
+++ scripts/package_usage_examples/seewave_example.R
@@ -2,6 +2,9 @@ library(seewave)
 library(tuneR)
 library(readr)
 library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(rgl)
 
 # Set working directory to input data folder
 setwd("~/birdinng/input/british-birdsong-dataset/")
@@ -16,17 +19,21 @@ ex_id <- song_meta %>%
   pull(file_id) ## Pull out file_id, store in ex_id
 
 # Choose example file:
-fname_base <- paste0("songs/xc",ex_id)
-fname_flac <- paste0(fname_base, ".flac")
-fname_wav <- paste0(fname_base, ".wav")
+ex_fname_base <- paste0("songs/xc",ex_id)
+ex_fname_flac <- paste0(fname_base, ".flac")
+ex_fname_wav <- paste0(fname_base, ".wav")
 
 # Convert .flac to .wav
 # Note: must download flac binary ($ brew install flac) before using wav2flac()
-seewave::wav2flac(fname_flac, reverse=T)
+seewave::wav2flac(ex_fname_flac, reverse=T)
 
 # Play .wav
 tuneR::setWavPlayer('/usr/bin/afplay')
-tuneR::play(fname_wav)
+tuneR::play(ex_fname_wav)
 
-ex_wave <- tuneR::readWave(fname_wav)
-ex_wave_mono <- mono(ex_wave, "left")
+# Read .wav into Wave object
+ex_wave <- tuneR::readWave(ex_fname_wav)
+
+# Construct spectrogram (y = Frequency, x = Time, color = Amplitude)
+ex_spectro <- seewave::spectro(ex_wave, flim=c(1,4.5))
+
