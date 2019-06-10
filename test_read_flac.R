849f32af02ee0530a9a5cb4171c8ec9b700bd9fe
Colin G. Wilson <colingwilson7@gmail.com>
2019-06-09 22:06:20 -0400

    Added British Birdsong Dataset (FLAC files and metadata CSV), tested conversion from FLAC to WAV

---
+++ test_read_flac.R
@@ -0,0 +1,33 @@
+library(seewave)
+library(tuneR)
+library(readr)
+library(dplyr)
+
+# Set working directory to input data folder
+setwd("~/birdinng/input/british-birdsong-dataset/")
+
+# Read in metadata
+song_meta <- read_csv("birdsong_metadata.csv")
+
+# Choose file_id
+ex_id <- song_meta %>%
+  filter(genus == "Picus") %>% ## European Green Woodpecker
+  slice(1) %>%
+  select(file_id) %>%
+  pull()
+
+# Choose example file:
+fname_base <- paste0("songs/xc",ex_id)
+fname_flac <- paste0(fname_base, ".flac")
+fname_wav <- paste0(fname_base, ".wav")
+
+# Convert .flac to .wav
+# Note: must download flac binary ($ brew install flac) before using wav2flac()
+seewave::wav2flac(fname_flac, reverse=T)
+
+# Play .wav
+tuneR::setWavPlayer('/usr/bin/afplay')
+tuneR::play(fname_wav)
+
+ex_wave <- tuneR::readWave(fname_wav)
+ex_wave_mono <- mono(ex_wave, "left")
